a2594506c4fb69ee98114cce0620dcb6b85345ae
Demetrio Lima <dlima.nyit@gmail.com>
2020-10-02 02:30:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-min_value = nil #gives the ability to set our first key's value as min_value
+min_value = nil #gives the ability to set our first key's value as our lowest value min_value
 current_key = nil #current_key will be filled once one of our conditions is met
 
 name_hash.each do |name, value|
